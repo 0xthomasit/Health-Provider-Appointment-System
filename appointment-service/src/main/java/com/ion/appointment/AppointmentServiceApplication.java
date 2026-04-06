7ce9c583d03afff9b6a84438012b6093fbd5e91d
Thomas Ngo <tuan.ngo.yj@renesas.com>
2026-04-06 15:34:41 +0700

    04 - Implement service-registry

--- appointment-service/src/main/java/com/ion/appointment/AppointmentServiceApplication.java
+++ appointment-service/src/main/java/com/ion/appointment/AppointmentServiceApplication.java
@@ -2,12 +2,14 @@ package com.ion.appointment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AppointmentServiceApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(AppointmentServiceApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(AppointmentServiceApplication.class, args);
+    }
 
 }
--- healthprovider-service/src/main/java/com/ion/healthprovider/HealthproviderServiceApplication.java
+++ healthprovider-service/src/main/java/com/ion/healthprovider/HealthproviderServiceApplication.java
@@ -2,11 +2,13 @@ package com.ion.healthprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HealthproviderServiceApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(HealthproviderServiceApplication.class, args);
     }
 
